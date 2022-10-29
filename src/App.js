f80e9849b817495e7eb9414bbd7ae038ce16b7dc
g0d13 <g0d13@outlook.com>
2022-10-29 12:26:01 -0500

    Add components library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
   );
 }
